cbcbb694de2c5a6d3d97648bd4274f4045bab59b
Zubin Juárez <zubinxxi@gmail.com>
2024-05-16 10:58:13 -0500

    Se crea las relaciones entre la tabla mensajes con propiedades y usuarios

--- models/index.js
+++ models/index.js
@@ -2,16 +2,22 @@ import Propiedad from './propiedad.js'
 import Precio from './precio.js'
 import Categoria from './categoria.js'
 import Usuario from './Usuario.js'
+import Mensaje from './mensaje.js'
+
 
 //Precio.hasOne(Propiedad) // Relacion uno a uno, pero se le de derecha a izquierda.
 Propiedad.belongsTo(Precio)
 Propiedad.belongsTo(Categoria)
 Propiedad.belongsTo(Usuario)
 
+Mensaje.belongsTo(Propiedad, {foreignKey: 'propiedadId'})
+Mensaje.belongsTo(Usuario, {foreignKey: 'usuarioId'})
+
 
 export {
     Propiedad,
     Precio,
     Categoria,
-    Usuario
+    Usuario,
+    Mensaje
 }
