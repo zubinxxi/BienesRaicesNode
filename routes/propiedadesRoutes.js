3349b1764db79a7a084463d93642d870b8e62570
Zubin Juárez <zubinxxi@gmail.com>
2024-05-29 11:48:34 -0500

    Se agrega la ruta para la página donde se muestran los mensajes.

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { body } from 'express-validator'
-import { admin, crear, guardar, agregarImagen, almacenarImagen, editar, guardarCambios, eliminar, mostrarPropiedad, enviarMensaje } from "../controllers/propiedadesController.js"
+import { admin, crear, guardar, agregarImagen, almacenarImagen, editar, guardarCambios, eliminar, mostrarPropiedad, enviarMensaje, verMensajes } from "../controllers/propiedadesController.js"
 import protegerRuta from '../middleware/protegerRuta.js'
 import upload from '../middleware/subirImagen.js'
 import identificarUsuario from '../middleware/identificarUsuario.js'
@@ -64,5 +64,6 @@ router.post(
     enviarMensaje
 )
 
+router.get('/mensajes/:id', protegerRuta, verMensajes)
 
 export default router 
