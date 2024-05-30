7585f98c4f9ec50914abfd0761c049f3b1df575a
Zubin Juárez <zubinxxi@gmail.com>
2024-05-30 11:16:19 -0500

    Se agrego el middleware para identificar al usuario a la ruta de inicio.

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,19 +1,20 @@
 import express from "express"
 import{inicio, categoria, noEncontrado, buscador} from '../controllers/appController.js'
+import identificarUsuario from '../middleware/identificarUsuario.js'
 
 //Crear la app
 const router = express.Router()
 
 // Página de inicio
-router.get('/', inicio)
+router.get('/', identificarUsuario, inicio)
 
 // Categorías
-router.get('/categoria/:id', categoria)
+router.get('/categoria/:id', identificarUsuario, categoria)
 
 //Página 404
 router.get('/404', noEncontrado)
 
 // Buscador
-router.post('/buscador', buscador)
+router.post('/buscador', identificarUsuario, buscador)
 
 export default router
